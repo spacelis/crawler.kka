1d3a46e7b105492cdcbe947e3d6197debc6c30ee
wenli <spacelis@gmail.com>
2014-02-12 17:08:52 +0100

    fixes for tweet_crawler

--- examples/tweet_crawler.py
+++ examples/tweet_crawler.py
@@ -13,9 +13,14 @@ Description:
 from gevent import monkey
 monkey.patch_all()
 
+import sys
 import yaml
 from tcrawl.actors import Controller
+from tcrawl.readers import CSVReader
+from tcrawl.writers import FileWriter
 import tweepy as tw
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 class Worker(object):
@@ -28,7 +33,12 @@ class Worker(object):
         :args: @todo
 
         """
-        self._client = tw.API(**creds)
+        self._oauthhl = tw.OAuthHandler(creds['consumer_key'],
+                                        creds['consumer_secret'])
+        self._oauthhl.set_access_token(creds['access_token_key'],
+                                       creds['access_token_secret'])
+        self._client = tw.API(self._oauthhl)
+        assert self._client.verify_credentials()
 
     def work_on(self, tid):
         """@todo: Docstring for work_on.
@@ -37,14 +47,20 @@ class Worker(object):
         :returns: @todo
 
         """
-        return self._client.get_status(id=tid)
+        try:
+            return self._client.get_status(id=tid)
+        except (TweepyError, AssertionError):
+            raise IgnorableError()
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print >> sys.stderr, 'Usage: tweet_crawler.py <input> <output>'
+        sys.exit()
 
-    with open('.cred.yaml') as fin:
+    with open('cred.yaml') as fin:
         cred = yaml.load(fin)
-    output = Collector(sys.argv[2])
     controller = Controller.start(Worker, cred,
-                                  CSVReader(sys.argv[1]), output)
+                                  CSVReader(sys.argv[1]),
+                                  FileWriter(sys.argv[2]))
     controller.actor_stopped.wait()
