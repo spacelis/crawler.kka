41adbb57b8dc4b7add21cfb467ffcfcaac273fe0
wenli <spacelis@gmail.com>
2014-02-19 12:04:40 +0100

    Many fixes for tweet id crawler.

--- examples/tweet_crawler.py
+++ examples/tweet_crawler.py
@@ -15,32 +15,41 @@ monkey.patch_all()
 
 import sys
 import yaml
+import time
+
+import tweepy as tw
 from tcrawl.actors import Controller
 from tcrawl.readers import CSVReader
 from tcrawl.writers import FileWriter
-import tweepy as tw
+from tcrawl import IgnorableError
+from tcrawl import RecoverableError
+
+from tcrawl.api_patch import patch as __patch_status
+__patch_status()
+
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
 
 
 class Worker(object):
 
     """ A worker. """
 
-    def __init__(self, creds):
+    def __init__(self, cred):
         """@todo: to be defined1.
 
         :args: @todo
 
         """
-        self._oauthhl = tw.OAuthHandler(creds['consumer_key'],
-                                        creds['consumer_secret'])
-        self._oauthhl.set_access_token(creds['access_token_key'],
-                                       creds['access_token_secret'])
+        self._oauthhl = tw.OAuthHandler(cred['consumer_key'],
+                                        cred['consumer_secret'])
+        self._oauthhl.set_access_token(cred['access_token_key'],
+                                       cred['access_token_secret'])
         self._client = tw.API(self._oauthhl)
-        assert self._client.verify_credentials()
+        assert self._client.verify_credentials(), cred
 
-    def work_on(self, tid):
+    def work_on(self, param):
         """@todo: Docstring for work_on.
 
         :tid: @todo
@@ -48,12 +57,22 @@ class Worker(object):
 
         """
         try:
-            return self._client.get_status(id=tid)
-        except (TweepyError, AssertionError):
-            raise IgnorableError()
+            ret = self._client.get_status(id=param['tid'])
+            _logger.debug(ret)
+            return ret._raw
+        except tw.TweepError as e:
+            if e.response.status == 429:
+                nxwin = e.response.getheader('X-Rate-Limit-Reset')
+                retry_in = time.time() - nxwin + 5
+                raise RecoverableError(e, retry_in)
+            raise IgnorableError(e)
 
 
-if __name__ == '__main__':
+def console():
+    """ running in console
+    :returns: @todo
+
+    """
     if len(sys.argv) != 3:
         print >> sys.stderr, 'Usage: tweet_crawler.py <input> <output>'
         sys.exit()
@@ -64,3 +83,6 @@ if __name__ == '__main__':
                                   CSVReader(sys.argv[1]),
                                   FileWriter(sys.argv[2]))
     controller.actor_stopped.wait()
+
+if __name__ == '__main__':
+    console()
